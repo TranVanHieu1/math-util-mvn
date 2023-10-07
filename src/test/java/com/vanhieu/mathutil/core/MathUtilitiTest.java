6f7195175d750eaa105cd6ef74a8fbec15f0f409
TranVanHieu1 <123tranhieu123@gmail.com>
2023-10-07 17:28:26 +0700

    upload screenshots folder

--- src/test/java/com/vanhieu/mathutil/core/MathUtilitiTest.java
+++ src/test/java/com/vanhieu/mathutil/core/MathUtilitiTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package co.vanhieu.mathutil.core;
+package com.vanhieu.mathutil.core;
 
 import com.vanhieu.mathutil.core.MathUtility;
 import org.junit.jupiter.api.Test;
