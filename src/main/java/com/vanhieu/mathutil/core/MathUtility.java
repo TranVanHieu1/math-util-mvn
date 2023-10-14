9b45c088c0c751453294965231d939803540216a
TranVanHieu1 <123tranhieu123@gmail.com>
2023-10-14 16:51:54 +0700

    Update getf() to use recursion.

--- src/main/java/com/vanhieu/mathutil/core/MathUtility.java
+++ src/main/java/com/vanhieu/mathutil/core/MathUtility.java
@@ -22,8 +22,36 @@ public class MathUtility {
     // n< 0> 20 ko tính do 21! tràn kiểu long
     //ko tính giai thừa âm!!!
     //0! = 1! = 1
-    public static long getFactorial(int n) throws IllegalAccessException{
-        long result = 1;
+//    public static long getFactorial(int n) throws IllegalAccessException{
+//        long result = 1;
+//        
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be betwen 0...20");
+//        
+//        
+//        if(n==0 || n== 1)
+//            return 1;
+//        
+//        for(int i = 2; i <= n; i++)
+//            result *= i;
+//            
+//        
+//        return result;
+//    }
+    
+    // Nâng cấp hàm tính giai thừa, dùng thuật toán đệ quy
+    
+    // n! = 1.2.3.4.5...
+    //5! = 1.2.3.4.5
+    // 5! = 4! x 5
+    // 4! = 3! x 4
+    // 3! = 2! X 3
+    // 2! = 2 X 1!
+    //1! là 1, dừng, không đuổi bắt
+    
+    // n! = n x ( n-1)!
+    // đệ quy == recursion: gọi lại chính mình với một quy mô nhỏ hơn
+     public static long getFactorial(int n) throws IllegalAccessException{
         
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be betwen 0...20");
@@ -32,10 +60,6 @@ public class MathUtility {
         if(n==0 || n== 1)
             return 1;
         
-        for(int i = 2; i <= n; i++)
-            result *= i;
-            
-        
-        return result;
+        return n * getFactorial(n-1); //n! = (n-1)!
     }
 }
